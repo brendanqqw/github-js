2374675ae308899607e2b5f6d005da26df8ba68c
brendanqqw <142297549+brendanqqw@users.noreply.github.com>
2024-02-23 07:30:00 +0000

    v1.1

---
+++ js/script.js
@@ -0,0 +1,20 @@
+import { generateTextAndImage} from "./utils.js"
+
+// 1. Change the value of the variable to your name
+let name = "Brendan Raul"
+
+// 2. Change the value of the variable to your favorite activity
+let favoriteActivity = "snacking"
+
+// 3. Assign the favoritePlace variable your favorite place
+// I.e. city, mountain, pub, forrest, beach, Manhattan, etc.
+let favoritePlace = "coffee shop"
+
+// 4. Configure the AI by setting a tempperature from 0 to 1
+// The higher temperature, the more random & expirmental output
+let temperature = 0.6
+
+//Optional: delete "avatar.jpg" and add a photo of yourself
+// (remember to use "avatar.jpg" as the name of your photo)
+
+generateTextAndImage(name, favoriteActivity, favoritePlace, temperature)
